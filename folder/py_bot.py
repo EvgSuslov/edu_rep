edb2755727467ab686a543bc4797e1be30fa35bb
EvgSuslov <94070733+EvgSuslov@users.noreply.github.com>
2022-06-16 11:54:43 +0300

    Update py_bot.py
    mesaege fixes

--- folder/py_bot.py
+++ folder/py_bot.py
@@ -1,14 +1,26 @@
 import telebot
 
-bot = telebot.TeleBot("###") # You can set parse_mode by default. HTML or MARKDOWN
+bot = telebot.TeleBot("№№№") # You can set parse_mode by default. HTML or MARKDOWN
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
+    bot.send_message(message, "Its my test bot")
+    
+    
     
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
+    
+    
+@bot.message_handler(commands=['button'])   
+def button_message(message):
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1=types.KeyboardButton("Кнопка")
+    bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
+
+
 
 bot.infinity_polling()
