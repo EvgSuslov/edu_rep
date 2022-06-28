9ca2811f14778a3bc25a767c88afc0ff1f5a9298
EvgSuslov <94070733+EvgSuslov@users.noreply.github.com>
2022-06-28 13:16:08 +0300

    Update untitled0.py

--- folder/untitled0.py
+++ folder/untitled0.py
@@ -1,28 +1,9 @@
-
-#Write a program to determine if the two given numbers
-# are coprime. A pair of numbers are 
-#coprime if their greatest shared factor is 1.
-
-#The inputs will always be two positive integers between 2 and 99.
-
-def are_coprime(n,m):
-    b=[]
-    c=[]
-    for i in range(1, 100):
-        if n % i == 0:
-            b.append(i)
-    for j in range(1, 100):
-        if m % j == 0:
-            c.append(j)
-    result = list(set(b) & set(c))
-
-    for number in result:
-        a = 0
-        if number >= a:
-            a = int(number)
-    if number == 1:
-        return True
+for x in range(1, 100):
+    if x%3 == 0 and x%5 == 0:
+        print("FizzBuzz")
+    elif x%3 == 0:
+        print("Fizz")
+    elif x%5 == 0:
+        print("Buzz")
     else:
-        return False
-    
-"аоаоао"
+        print(x) 
