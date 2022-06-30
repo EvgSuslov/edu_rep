45b3275484095cef4a5da6091ef2c8d5b8c8eb50
EvgSuslov <94070733+EvgSuslov@users.noreply.github.com>
2022-06-30 22:07:52 +0300

    Update untitled0.py

--- folder/untitled0.py
+++ folder/untitled0.py
@@ -1,9 +1,9 @@
 for x in range(1, 100):
     if x%3 == 0 and x%5 == 0:
-        print("FizzBuzz")
+        print("FizzBuzz", end = ' ')
     elif x%3 == 0:
-        print("Fizz")
+        print("Fizz", end = ' ')
     elif x%5 == 0:
-        print("Buzz")
+        print("Buzz", end = ' ')
     else:
-        print(x) 
+        print(x, end = ' ') 
