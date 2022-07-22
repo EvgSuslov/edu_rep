7c92618c8381aa2d106abca02565db974e8f7058
EvgSuslov <94070733+EvgSuslov@users.noreply.github.com>
2022-07-22 15:10:38 +0300

    Update untitled0.py
    fuel task

--- folder/untitled0.py
+++ folder/untitled0.py
@@ -1,9 +1,17 @@
-for x in range(1, 100):
-    if x%3 == 0 and x%5 == 0:
-        print("FizzBuzz")
-    elif x%3 == 0:
-        print("Fizz")
-    elif x%5 == 0:
-        print("Buzz")
-    else:
-        print(x) 
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 22 14:03:38 2022
+
+fuel calculator 
+
+@author: ruiny
+"""
+
+range_ = float(input('Расстояние до цели'+'\n'))
+fuel_amaunt = float(input("рАСХОД НА 100"+'\n'))
+fuel_cost = float(input('сТОЙМОСТЬ ЗА ЛИТР'+'\n'))
+total = 0
+if range_ > 0 and fuel_amaunt > 0 and fuel_cost > 0:
+    total = ((fuel_amaunt / 100) * range_)  * fuel_cost
+    
+print('цена за поезлдку \n '+ str(total))
