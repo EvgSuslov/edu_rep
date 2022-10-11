b460d2e3e3754646e5a00b14709d862a6aeeb781
EvgSuslov <94070733+EvgSuslov@users.noreply.github.com>
2022-10-11 15:21:59 +0300

    Create Total_amount_of_points.py

---
+++ folder/Total_amount_of_points.py
@@ -0,0 +1,18 @@
+def points(games):
+    a=0
+    for i in games:
+        if i[0] > i[2]: #3 points (win)
+            a+=3
+        elif i[0] < i[2]: #0 points (loss)
+            a+=0
+        elif i[0] == i[2]: # 1 point (tie)
+            a+=1
+        
+    return a
+    
+    basic_test_cases():
+        test.assert_equals(points(['1:0','2:0','3:0','4:0','2:1','3:1','4:1','3:2','4:2','4:3']), 30)
+        test.assert_equals(points(['1:1','2:2','3:3','4:4','2:2','3:3','4:4','3:3','4:4','4:4']), 10)
+        test.assert_equals(points(['0:1','0:2','0:3','0:4','1:2','1:3','1:4','2:3','2:4','3:4']), 0)
+        test.assert_equals(points(['1:0','2:0','3:0','4:0','2:1','1:3','1:4','2:3','2:4','3:4']), 15)
+        test.assert_equals(points(['1:0','2:0','3:0','4:4','2:2','3:3','1:4','2:3','2:4','3:4']), 12)
