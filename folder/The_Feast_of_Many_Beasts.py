403772e787153f3ee851058e4064181d72076182
EvgSuslov <94070733+EvgSuslov@users.noreply.github.com>
2022-10-11 13:51:13 +0300

    Create The_Feast_of_Many_Beasts.py

---
+++ folder/The_Feast_of_Many_Beasts.py
@@ -0,0 +1,26 @@
+def feast(beast, dish):
+    a=b=''    
+    a+=str(beast[0])+str(beast[(len(beast)-1)])
+    b+=str(dish[0])+str(dish[(len(dish)-1)])
+    if a == b:
+        return True
+    else:
+        return False
+      
+def basic_test_cases():
+  test.assert_equals(feast("great blue heron", "garlic naan"), True)
+  test.assert_equals(feast("chickadee", "chocolate cake"), True)
+  test.assert_equals(feast("brown bear", "bear claw"), False)
+
+# All of the animals are having a feast! Each animal is bringing one dish.
+# There is just one rule: the dish must start and end with the same letters 
+# as the animal's name. For example, the great blue heron is bringing garlic
+# naan and the chickadee is bringing chocolate cake.
+#
+# Write a function feast that takes the animal's name and dish as arguments and 
+# returns true or false to indicate whether the beast is allowed to bring the dish 
+# to the feast.
+#
+# Assume that beast and dish are always lowercase strings, and that each has at least 
+# two letters. beast and dish may contain hyphens and spaces, but these will not appear
+# at the beginning or end of the string. They will not contain numerals.
